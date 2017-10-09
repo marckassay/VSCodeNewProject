9837d05b2e70b7df1547a99cb25d1d0ac8478f83
Marc Kassay <marckassay@gmail.com>
2017-10-09 15:14:28 -0400

    Adding html, scripts, stlyes and images tasks to '.vscode/tasks.json' that will be watched when changed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,48 @@
-var gulp = require("gulp");
-var gutil = require("gulp-util");
+'use strict';
+var gulp = require('gulp');
+var del = require('del');
+
 var paths = {
-    pages: ['src/**']
+    html: './src/*.html',
+    scripts: ['./src/js/**/*.js'],
+    styles: './src/css/**/*',
+    images: './src/img/**/*'
 };
 
-gulp.task("src", function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
+gulp.task('clean', function() {
+    return del(['build']);
 });
 
-gulp.task("default", ["src"]);
+gulp.task("update-html", ['clean'], function () {
+    return gulp.src(paths.html, { base: 'src' })
+        .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("update-scripts", ['clean'], function () {
+    return gulp.src(paths.scripts, { base: 'src' })
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task("update-styles", ['clean'], function () {
+    return gulp.src(paths.styles, { base: 'src' })
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('update-images', ['clean'], function() {
+    return gulp.src(paths.images, { base: 'src' })
+        .pipe(gulp.dest('./dist'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(paths.html, ['update-html']);
+    gulp.watch(paths.scripts, ['update-scripts']);
+    gulp.watch(paths.styles, ['update-styles']);
+    gulp.watch(paths.images, ['update-images']);
+});
+
+gulp.task('default', ['watch',
+                      'update-html',
+                      'update-scripts',
+                      'update-styles',
+                      'update-images']);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 function myFunction() {
     var x = document.getElementById("demo");
     x.style.fontSize = "25px"; 
-    x.style.color = "red"; 
+    x.style.color = "green"; 
 }
